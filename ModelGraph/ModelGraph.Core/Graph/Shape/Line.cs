2455a8b92d42a3c5152e0cb1e569a7fc9e4c4859
David Tetzlaff <davetz@outlook.com>
2020-10-17 13:20:53 -0600

    Corrected Line extent

--- ModelGraph/ModelGraph.Core/Graph/Shape/Line.cs
+++ ModelGraph/ModelGraph.Core/Graph/Shape/Line.cs
@@ -28,7 +28,14 @@ namespace ModelGraph.Core
         #region OverideAbstract  ==============================================
         internal override Shape Clone() => new Line(this);
         internal override Shape Clone(Vector2 center) => new Line(this, center);
-        protected override ShapeProperty PropertyFlags => ShapeProperty.Horz;
+        protected override ShapeProperty PropertyFlags => ShapeProperty.Cent;
+        protected override (float dx1, float dy1, float dx2, float dy2) GetExtent()
+        {
+            var (dx1, dy1, dx2, dy2) = base.GetExtent();
+            var dw = SW / 100f;
+            if (SW == 0) dw = .01f;
+            return (dx1 - dw, dy1 - dw, dx2 + dw, dy2 + dw);
+        }
         #endregion
     }
 }
